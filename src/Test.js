92e6d691c3014ad4fde5588946d360f1ffa9e8b6
Jesse Gray <jessegray@jesses-mbp.home>
2023-09-27 15:20:49 -0400

    still testing build

---
+++ src/Test.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Test extends Component{
+    render(){
+        return(
+            <div>
+                <p>testing new azure react app</p>
+            </div>
+        )
+    }
+}
