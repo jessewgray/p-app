a73006d79153d0836a1417954fe638128d9b9ddb
Jesse Gray <jessegray@jesses-mbp.home>
2023-10-10 12:06:00 -0400

    testing another push

--- src/Test.js
+++ src/Test.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react'
 
 class Test extends Component{
+
     render(){
+        let getData = function(){
+           
+            //fetch('http://localhost:8080/get')
+            fetch('https://backend-pigeon.azurewebsites.net/get')
+                    .then(response => response.json())
+                    .then((data) => {
+                        console.log(data)
+                    })
+                    .catch(error => console.error(error));    
+           }
+           
         return(
             <div>
                 <p>testing new azure react app</p>
