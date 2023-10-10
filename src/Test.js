dfb838f902c6125042d44d2acc1673fd855d8af2
Jesse Gray <jessegray@jesses-mbp.home>
2023-10-10 12:10:42 -0400

    back to oct

--- src/Test.js
+++ src/Test.js
@@ -3,21 +3,10 @@ import React, {Component} from 'react'
 class Test extends Component{
 
     render(){
-        let getData = function(){
-           
-            //fetch('http://localhost:8080/get')
-            fetch('https://backend-pigeon.azurewebsites.net/get')
-                    .then(response => response.json())
-                    .then((data) => {
-                        console.log(data)
-                    })
-                    .catch(error => console.error(error));    
-           }
-           
         return(
             <div>
                 <p>testing new azure react app</p>
-                <p>october 2023</p>
+                <p>october</p>
             </div>
         )
     }
